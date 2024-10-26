202c90b609464f57fcd617ac8d95730b590dd464
AmeerMuhsin <44871551+AmeerMuhsin@users.noreply.github.com>
2024-10-26 15:02:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+    const form = document.getElementById('form'); 
+    const name1 = document.getElementById('name1');
+    const email = document.getElementById('email');
+    const phonenumber = document.getElementById('phonenumber');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        validateInputs();
+    });
+
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success')
+    }
+
+    const setSuccess = element =>{
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error')
+    }
+
+
+    const isValidEmail = email => {
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+        return re.test(String(email).toLowerCase())
+    };
+       
+    const validateInputs = () =>{
+        const nameValue = name1.value.trim();
+        const emailValue = email.value.trim();
+        const phonenumberValue = phonenumber.value.trim();        
+
+        if(nameValue === '' || nameValue == null){
+            setError(name1,'Username is required');
+        } else{
+            setSuccess(name1)
+        }
+
+        if(emailValue === ''){
+            setError(email,'Email is required');}
+            else if(!isValidEmail(emailValue)){
+                setError(email,'Proper email is required');}
+            else{
+                setSuccess(email);}
+
+        if(phonenumberValue === ''){
+            setError(phonenumber,'Phone Number is required')}
+        else if(phonenumberValue.length < 10){ 
+            setError(phonenumber,'Phone Number must be 10 character')}
+        else{
+            setSuccess(phonenumber);}
+    };
